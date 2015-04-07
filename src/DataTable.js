b8edbe7b4308f1c3f5bf3f19c03eacbae992cb39
Timothy Chon <devchon+github@gmail.com>
2015-04-07 15:28:01 -0700

    place page size and pagination beneath table
      * TODO: add configure to do this

--- src/DataTable.js
+++ src/DataTable.js
@@ -19,26 +19,11 @@ var DataTable = React.createClass({
       <div className={this.props.className}>
         <div className="row">
           <div className="col-xs-4">
-            <SelectField
-              id="page-menu"
-              label="Page size:"
-              value={this.state.pageLength}
-              options={this.props.pageLengthOptions}
-              onChange={this.onPageLengthChange}
-            />
             <SearchField
               id="search-field"
               label="Search:"
               value={this.state.filterValues.globalSearch}
               onChange={this.onFilter.bind(this, 'globalSearch')}
-            />
-          </div>
-          <div className="col-xs-8">
-            <Pagination
-              className="pagination pull-right"
-              currentPage={page.currentPage}
-              totalPages={page.totalPages}
-              onChangePage={this.onChangePage}
             />
           </div>
         </div>
@@ -50,6 +35,25 @@ var DataTable = React.createClass({
           sortBy={this.state.sortBy}
           onSort={this.onSort}
         />
+        <div className="row">
+          <div className="col-xs-4">
+            <SelectField
+              id="page-menu"
+              label="Page size:"
+              value={this.state.pageLength}
+              options={this.props.pageLengthOptions}
+              onChange={this.onPageLengthChange}
+            />
+          </div>
+          <div className="col-xs-8">
+            <Pagination
+              className="pagination pull-right"
+              currentPage={page.currentPage}
+              totalPages={page.totalPages}
+              onChangePage={this.onChangePage}
+            />
+          </div>
+        </div>
       </div>
     );
   }
